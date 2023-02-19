e3ea2f09fe4ea5d58747ca5d551a4f98a2cc6a9a
Ricky <ricardo.d.r.lopes@gmail.com>
2023-02-19 15:59:49 +0100

    first commit

---
+++ variables.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	x := 10
+
+	var b, c int = 1, 2
+	fmt.Println(b, c, x)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+
+	var g string = "peach"
+	fmt.Println(g)
+
+	var h = "banana"
+	fmt.Println(h)
+
+}
